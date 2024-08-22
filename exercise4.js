d4654fe6ea504c05301fd0fc455c89b5fdd6406b
Angekarara <mbabange2020@gmail.com>
2024-08-22 08:50:28 +0200

    improved codes of exercise 4

--- exercise4.js
+++ exercise4.js
@@ -1,10 +1,4 @@
 function findEvenNumbers(numbers) {
-    var evenNumbers = [];
-    for (var i = 0; i < numbers.length; i++) {
-      if (numbers[i] % 2 === 0) {
-        evenNumbers.push(numbers[i]);
-      }
-    }
-    return evenNumbers;
-  }
-  console.log(findEvenNumbers([5,6,1,3]))
+  return numbers.filter((number) => number % 2 === 0);
+}
+console.log(findEvenNumbers([5, 6, 2, 1, 3]));
