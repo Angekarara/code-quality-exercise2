60f87d88e5ba98307b5ed4ae6a086c5bef1fc8c3
Angekarara <mbabange2020@gmail.com>
2024-08-22 08:59:55 +0200

    improved codes of exercise 5

--- exercise5.js
+++ exercise5.js
@@ -1,12 +1,5 @@
 function calculateFactorial(n) {
-  if (n === 0 || n === 1) {
-    return 1;
-  } else {
-    var factorial = 1;
-    for (var i = 2; i <= n; i++) {
-      factorial *= i;
-    }
-    return factorial;
-  }
+  if (n === 0 || n === 1) return 1;
+  return n * calculateFactorial(n - 1);
 }
-console.log(calculateFactorial(6))
+console.log(calculateFactorial(6));
