f6586d5995ed96aaaf81b51b62c997198119890a
Angekarara <mbabange2020@gmail.com>
2024-08-22 08:28:45 +0200

    first codes

---
+++ exercise1.js
@@ -0,0 +1,11 @@
+function calculateAverage(numbers) {
+    var sum = 0;
+    var count = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+      count++;
+    }
+    var average = sum / count;
+    return average;
+  }
+  console.log(calculateAverage([3,6,9,4]))
