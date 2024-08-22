b93e24b3266fa57b117a7ded9ef5380a6890b3d4
Angekarara <mbabange2020@gmail.com>
2024-08-22 09:19:19 +0200

    improved codes of exercise 8

--- exercise8.js
+++ exercise8.js
@@ -1,10 +1,4 @@
 function discount(totalamount) {
-    var disc;
-    if (totalamount > 100) {
-      disc = totalamount * 0.1;
-    } else {
-      disc = totalamount * 0.05;
-    }
-    return disc;
-  }
-  console.log(discount(679))
+  return totalamount > 100 ? totalamount * 0.1 : totalamount * 0.05;
+}
+console.log(discount(679));
