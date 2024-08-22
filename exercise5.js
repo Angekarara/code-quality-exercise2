e058353ee95e760cda30cfb7c3b096e2c7c786d9
Angekarara <mbabange2020@gmail.com>
2024-08-22 08:53:51 +0200

    first codes of exercise 5

---
+++ exercise5.js
@@ -0,0 +1,12 @@
+function calculateFactorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    var factorial = 1;
+    for (var i = 2; i <= n; i++) {
+      factorial *= i;
+    }
+    return factorial;
+  }
+}
+console.log(calculateFactorial(6))
